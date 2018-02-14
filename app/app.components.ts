815ad4637f8b8a542a9c996436ed95bbfda26252
Andre Ricardo Alves <andrecajuru@gmail.com>
2018-02-14 10:51:44 -0200

    Projeto Adicionado

---
+++ app/app.components.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html'
+})
+export class AppComponent{
+   
+    log(param:string):void{
+        console.log(param);
+    }
+}
---
+++ app/in-memory-data.service.js
@@ -0,0 +1,17 @@
+"use strict";
+class InMemoryDataService {
+    createDb() {
+        let contatos = [
+            { id: 1, nome: 'Andre', email: 'andre@gmail.com', telefone: '(11) 11111-1111' },
+            { id: 2, nome: 'Ingrid', email: 'ingrid@gmail.com', telefone: '(22) 22222-2222' },
+            { id: 3, nome: 'Ana', email: 'ana@gmail.com', telefone: '(33) 33333-3333' },
+            { id: 4, nome: 'Paula', email: 'paula@gmail.com', telefone: '(44) 44444-4444' },
+            { id: 5, nome: 'Anderson', email: 'anderson@gmail.com', telefone: '(55) 55555-5555' },
+            { id: 7, nome: 'Maria', email: 'maria@gmail.com', telefone: '(77) 77777-77777' },
+            { id: 8, nome: 'Ana Flávia', email: 'anaflavia@gmail.com', telefone: '(88) 88888-88888' },
+        ];
+        return { contatos };
+    }
+}
+exports.InMemoryDataService = InMemoryDataService;
+//# sourceMappingURL=in-memory-data.service.js.map
